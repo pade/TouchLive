cca7b6a4800cd57d6b07157f5f07b2f4043a9660
Leo <leooooo@bu.edu>
2016-07-14 15:55:32 -0400

    seems to be working ^-^

--- main.py
+++ main.py
@@ -16,8 +16,9 @@ from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.button import Button
 from kivy.uix.slider import Slider
 from kivy.properties import ObjectProperty, BooleanProperty, NumericProperty
-from simpleOSC import initOSCClient, initOSCServer, closeOSC, \
-        setOSCHandler, sendOSCMsg
+from newOSC import TouchLiveOscBackend
+# from simpleOSC import initOSCClient, initOSCServer, closeOSC, \
+#         setOSCHandler, sendOSCMsg
 import pygame.midi
 
 
@@ -50,19 +51,19 @@ class TouchLiveMain(FloatLayout):
 
     def __init__(self, **kwargs):
         super(TouchLiveMain, self).__init__(**kwargs)
-
+        self.newOSC = TouchLiveOscBackend()
         # create and bind every slider and buttons
-        for x in xrange(64):
+        for x in range(64):
             btn = TouchLiveButton(index=x)
             btn.bind(state=self.on_button_state)
             self.grid.add_widget(btn)
 
-        for x in xrange(16):
+        for x in range(16):
             slider = TouchLiveSlider(index=x, min=0, max=127)
             slider.bind(value=self.on_right_slider_value)
             self.grid_right.add_widget(slider)
 
-        for x in xrange(8):
+        for x in range(8):
             slider = TouchLiveSlider(index=x, min=0, max=127,
                                         orientation='vertical')
             slider.bind(value=self.on_bottom_slider_value)
@@ -70,25 +71,25 @@ class TouchLiveMain(FloatLayout):
 
         # led handler for monome
         prefix = self.app.config.get('monome', 'prefix')
-        setOSCHandler('/%s/led' % prefix, self.on_led)
-        setOSCHandler('/%s/led_col' % prefix, self.on_led_col)
-        setOSCHandler('/%s/led_row' % prefix, self.on_led_row)
-        setOSCHandler('/%s/led_frame' % prefix, self.on_led_frame)
-        setOSCHandler('/%s/led_clear' % prefix, self.on_led_clear)
+        self.newOSC.setOSCHandler('/%s/led' % prefix, self.on_led)
+        self.newOSC.setOSCHandler('/%s/led_col' % prefix, self.on_led_col)
+        self.newOSC.setOSCHandler('/%s/led_row' % prefix, self.on_led_row)
+        self.newOSC.setOSCHandler('/%s/led_frame' % prefix, self.on_led_frame)
+        self.newOSC.setOSCHandler('/%s/led_clear' % prefix, self.on_led_clear)
 
         # empty handlers
-        setOSCHandler('/sys/prefix', self.empty_handler)
-        setOSCHandler('/sys/cable', self.empty_handler)
-        setOSCHandler('/sys/offset', self.empty_handler)
-        setOSCHandler('/sys/intensity', self.empty_handler)
-        setOSCHandler('/sys/test', self.empty_handler)
-        setOSCHandler('/sys/report', self.empty_handler)
+        self.newOSC.setOSCHandler('/sys/prefix', self.empty_handler)
+        self.newOSC.setOSCHandler('/sys/cable', self.empty_handler)
+        self.newOSC.setOSCHandler('/sys/offset', self.empty_handler)
+        self.newOSC.setOSCHandler('/sys/intensity', self.empty_handler)
+        self.newOSC.setOSCHandler('/sys/test', self.empty_handler)
+        self.newOSC.setOSCHandler('/sys/report', self.empty_handler)
 
     def on_button_state(self, instance, value):
         prefix = self.app.config.get('monome', 'prefix')
         index = instance.index
         value = 1 if value == 'down' else 0
-        sendOSCMsg('/%s/press' % prefix, [index % 8, index / 8, value])
+        self.newOSC.sendOSCMsg('/%s/press' % prefix, [index % 8, index / 8, value])
 
     def on_bottom_slider_value(self, instance, value):
         midi_out = self.app.midi_out
@@ -106,32 +107,32 @@ class TouchLiveMain(FloatLayout):
 
     def on_led_col(self, addr, tags, data, source):
         col, cv = data[0:2]
-        colvals = map(int, (list(''.join([str((cv>> y) & 1) for y in xrange(7, -1, -1)]))))
+        colvals = map(int, (list(''.join([str((cv>> y) & 1) for y in range(7, -1, -1)]))))
         colvals.reverse()
-        for i in xrange(8):
+        for i in range(8):
             index = int(col + (8 * i))
             self.grid.children[index].highlight = (colvals[i] > 0)
 
     def on_led_row(self, addr, tags, data, source):
         row, rv = data[0:2]
-        rowvals = map(int, (list(''.join([str((rv>> y) & 1) for y in xrange(7, -1, -1)]))))
+        rowvals = map(int, (list(''.join([str((rv>> y) & 1) for y in range(7, -1, -1)]))))
         rowvals.reverse()
-        for i in xrange(8):
+        for i in range(8):
             index = int(row) * 8 + i
             self.grid.children[index].highlight = (rowvals[i] > 0)
 
     def on_led_frame(self, addr, tags, data, source):
-        for i in xrange(8):
+        for i in range(8):
             rv = data[i]
             rowvals = (map(int, (list("".join([str((rv>> y) & 1) for y in range(7, -1, -1)])))))
             rowvals.reverse()
-            for n in xrange(8):
+            for n in range(8):
                 index = i * 8 + n
                 self.grid.children[index].highlight = rowvals[n] > 0
 
     def on_led_clear(self, addr, tags, data, source):
         x = data[0]
-        for i in xrange(64):
+        for i in range(64):
             self.grid.children[i] = (x == 1)
 
     def empty_handler(self, *largs):
@@ -187,7 +188,7 @@ class TouchLiveApp(App):
         settings.add_json_panel('Touch Live', self.config, data=data)
 
     def on_stop(self):
-        closeOSC()
+        self.root.newOSC.closeOSC()
 
     def do_connect(self):
         config = self.config
@@ -195,9 +196,6 @@ class TouchLiveApp(App):
         sport = config.getint('monome', 'send_port')
         rport = config.getint('monome', 'receive_port')
         mdevice = config.getint('midi', 'device')
-        # osc
-        initOSCClient(host, sport)
-        initOSCServer(host, rport)
         # midi
         pygame.midi.init()
         pygame.midi.get_count()
@@ -208,6 +206,11 @@ class TouchLiveApp(App):
         parent = self.root.parent
         parent.remove_widget(self.root)
         self.root = TouchLiveMain(app=self)
+        
+        # osc
+        self.root.newOSC.initOSCClient(host, sport)
+        self.root.newOSC.initOSCServer(host, rport)
+
         parent.add_widget(self.root)
 
 
@@ -215,4 +218,5 @@ if __name__ in ('__main__', '__android__'):
     try:
         TouchLiveApp().run()
     finally:
-        closeOSC()
+        print('bye')
+        #closeOSC()
